8f228338375d232f786e7de47d7fbad3dd716671
kirill3pt <kirill.dib9@yandex.ru>
2026-01-11 01:47:26 +0300

    Задание №1, лабораторная работа №1

--- LAB1. Python Basics/LAB1. Python Basics/LAB1._Python_Basics.py
+++ LAB1. Python Basics/LAB1. Python Basics/LAB1._Python_Basics.py
@@ -1,5 +1,14 @@
-print("Input a & b");
-a = int(input("a = "))
-b = int(input("b = "))
-s = a + b
-print("Sum: ", s)
+﻿while True:
+    print("1 - задание №1")
+    print("2 - задание №2")
+    print("0 - выход")
+    ch = input("Выберите задание: ")
+    if ch == "1":
+        money = float(input("Введите сумму(через точку): "))
+        rubles = int(money)
+        cents = int(round((money - rubles) * 100))
+        print(f"{rubles} руб. {cents:02d} копеек ")
+    elif ch == "0": 
+        exit()
+    else: 
+        print("This choice is invalid. ")
