7a187bb566c5957dd2c75be6b29c8b05c905df7a
kirill3pt <kirill.dib9@yandex.ru>
2026-01-18 01:59:46 +0300

    изменения в main.py

--- LAB1. Python Basics/LAB1. Python Basics/main.py
+++ LAB1. Python Basics/LAB1. Python Basics/main.py
@@ -1,21 +1,29 @@
 ﻿import task1
 import task2
 import task3
+import task4
+import task5
+import task6
 
 def main():
     while True:
         print("1 - перевод в деньги")
         print("2 - проверка на возрастание")
         print("3 - номер карты")
+        print("4 - разделение по словам")
         print("0 - выход")
         choice = input("Введите номер задания: ")
         if choice == "1":
             task1.convertToMoney()
         elif choice == "2":
-            arr = [1, 15, 30, 45, 60]
-            print(task2.increas(arr))
+            arrTRUE = [1, 15, 30, 45, 60]
+            arrFALSE = [2, 1, 15, 10, 7]
+            print(task2.increas(arrTRUE))
+            print(task2.increas(arrFALSE))
         elif choice == "3":
             task3.numbers()
+        elif choice == "4":
+            task4.splits()
         elif choice == "0":
             break
         else:
