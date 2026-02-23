5ce5c8c9c8c2747b0e2c1ad7d720a2bc464cf03d
kirill3pt <kirill.dib9@yandex.ru>
2026-02-23 23:45:35 +0300

    PEP8 переделка (попытка)

--- LAB1. Python Basics/LAB1. Python Basics/task16.py
+++ LAB1. Python Basics/LAB1. Python Basics/task16.py
@@ -12,22 +12,22 @@ def UEFA_Champions_League():
     groups = [teams[i * 4:(i + 1) * 4] for i in range(4)] #формовка 4 групп по 4 команды
 
     year = datetime.now().year
-    hour = random.randint(12, 22) #часы с 12 до 22
-    minute = random.choice([0, 15, 30, 45]) #минуты: 0, 15, 30, 45
-    start_date = datetime(year, 9, 14, hour, minute)
+    match_hours = [12, 14, 16, 18, 20, 22]
+    match_minutes = [0, 15, 30, 45]
+    start_date = datetime(year, 9, 14)
 
     for idx, group in enumerate(groups, 1): #вывод групп
         print("Группа {}: {}".format(idx, group))
 
-    for idx, group in enumerate(groups, 1): #составление календаря игр
-        print("Календарь игр для Группы {}:".format(idx))
-        matches = list(itertools.combinations(group, 2))
-        match_date = datetime(year, 9, 14)  #дата первой игры в группе
-        times = [12, 14, 16, 18, 20, 22]  #возможные часы игр
-        for match in matches:
-            team1, team2 = match
-            hour = random.choice(times)
-            minute = random.choice([0, 15, 30, 45])
-            current_match = match_date.replace(hour=hour, minute=minute) #для того чтобы время матчей было разное
-            print(f"{current_match.strftime('%d/%m/%Y, %H:%M')} — {team1} vs {team2}") #dd/mm/yyyy HH:MM
-            match_date += timedelta(weeks=2) 
+    for idx, group in enumerate(groups, 1):
+        print(f"Календарь игр для Группы {idx}:")
+        match_date = start_date
+
+        for team1, team2 in itertools.combinations(group, 2):
+            # выбираем случайное время для каждого матча
+            current_match = match_date.replace(
+                hour=random.choice(match_hours),
+                minute=random.choice(match_minutes)
+            )
+            print(f"{current_match.strftime('%d/%m/%Y, %H:%M')} — {team1} vs {team2}")
+            match_date += timedelta(weeks=2)
