cc115a7d13a0273fda67f466eb8caa5fe6c06158
kirill3pt <kirill.dib9@yandex.ru>
2026-03-24 18:37:08 +0300

    исправлено задание №2

--- LAB2. Python files and OS/LAB2. Python files and OS/task2.py
+++ LAB2. Python files and OS/LAB2. Python files and OS/task2.py
@@ -1,21 +1,31 @@
 ﻿from pathlib import Path
+import hashlib
 
 def second():
     folder = input("Введите путь к директории: ")
     files_data = {}
+
     for path in Path(folder).rglob("*"):
         if path.is_file():
             try:
-                content = path.read_bytes()
-    
-                if content in files_data:
-                    files_data[content].append(str(path))
+                md5 = hashlib.md5()
+
+                with open(path, "rb") as f:
+                    while chunk := f.read(4096):
+                        md5.update(chunk)
+
+                file_hash = md5.hexdigest()
+
+                if file_hash in files_data:
+                    files_data[file_hash].append(str(path))
                 else:
-                    files_data[content] = [str(path)]
-            except:
-                pass
-    
+                    files_data[file_hash] = [str(path)]
+
+            except Exception as e:
+                print(f"Ошибка с файлом {path}: {e}")
+
     print("Найденные дубликаты:\n")
+
     for paths in files_data.values():
         if len(paths) > 1:
             print("Группа:")
